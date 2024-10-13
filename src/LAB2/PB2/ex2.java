f49c6a386436e27d11565c4b5dd7a140a6ddaa24
alexgh05 <alex.ghenciu05@gmail.com>
2024-10-13 15:41:24 +0300

    Tema PJ2

--- src/LAB2/PB2/ex2.java
+++ src/LAB2/PB2/ex2.java
@@ -1,7 +1,27 @@
 package LAB2.PB2;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class ex2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        String filename="C:\\Users\\alexg\\IdeaProjects\\Tema PJ\\src\\cantec_in.txt";
+        try (Scanner sc = new Scanner(new File(filename))) {
+            OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream("cantec_out.txt"));
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                String[] words = line.split(" ");
+                int word_number= words.length;
+
+                osw.write(line+" "+word_number+"\n");
+
+            }
+            osw.close();
+        }
+        catch (FileNotFoundException e) {} catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
 }
